4df912f8a3086e88f7f53d96c30ee4c3f6d729d2
Daniel Marklund <daniel.marklund@tieto.com>
2015-04-15 15:28:03 +0200

    Added some code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,8 +12,22 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
+		for (int ix = 0; ix < args.length; ix++) {
+			System.out.println("Arg " + ix + ": " + args[ix]);
+		}
+		Welcomer www = new Welcomer();
+		www.sayHi();
 	}
 
+}
+
+class Welcomer {
+	private String welcom = "Hello!";
+	
+	public void sayHi() {
+		System.out.println(welcom);
+	}
+	
+	
 }
